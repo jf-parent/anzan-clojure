b14f7971d21847319381aa005826564cd0d2dd29
parent-j-f <parent.j.f@gmail.com>
2021-01-10 12:38:02 -0500

    Retry; config saved to local storage; global handler;

--- src/cljs/anzan_clojure/core.cljs
+++ src/cljs/anzan_clojure/core.cljs
@@ -1,37 +1,45 @@
 (ns anzan-clojure.core
   (:require
-   [reagent.core :as reagent :refer [atom]]
+   [reagent.core :as reagent]
    [reagent.dom :as rdom]
+   [alandipert.storage-atom :refer [local-storage]]
    [reagent.session :as session]
    [reitit.frontend :as reitit]
    [clerk.core :as clerk]
    [accountant.core :as accountant]))
 
-(enable-console-print!)
+;; (enable-console-print!)
+
+;; -------------------------
+;; Local storage
+
+(def current-config (local-storage (atom {}) :current-config))
 
 ;; -------------------------
 ;; State
 
-(def number-of-digits (atom 3))
-(def number-of-rows (atom 3))
-(def number-shown-ms (atom 1000))
-(def timeout (atom 1000))
-(def ask-answer (atom false))
-(def user-answer (atom ""))
-(def numbers (atom []))
-(def current-number (atom 0))
-(def current-config (atom {}))
-(def current-row (atom 0))
-(def current-answer (atom 0))
-(def history-total-question (atom 0))
-(def history-total-right-answer (atom 0))
-(def last-answer-correct (atom 0))
+(def number-of-digits (reagent/atom (:number-of-digits @current-config 3)))
+(def number-of-rows (reagent/atom (:number-of-rows @current-config 3)))
+(def number-shown-ms (reagent/atom (:number-shown-ms @current-config 1000)))
+(def timeout (reagent/atom (:timeout @current-config 1000)))
+(def ask-answer (reagent/atom false))
+(def user-answer (reagent/atom ""))
+(def numbers (reagent/atom []))
+(def current-number (reagent/atom 0))
+(def current-row (reagent/atom 0))
+(def current-answer (reagent/atom 0))
+(def history-total-question (reagent/atom 0))
+(def history-total-right-answer (reagent/atom 0))
+(def last-answer-correct (reagent/atom 0))
+(def running (reagent/atom false))
 
 ;; -------------------------
 ;; Functions
 
 (defn get-number [nb-of-digit]
-  (apply str (repeatedly nb-of-digit #(inc (rand-int 8)))))
+  (if (< @current-row (count @numbers))
+    (nth @numbers @current-row)
+    (apply str (repeatedly nb-of-digit #(inc (rand-int 9))))))
 
 (defn hide-number []
   (reset! current-number 0))
@@ -46,21 +54,27 @@
         (js/setTimeout main-loop (@current-config :timeout)))
     (do (reset! current-row 0)
         (reset! last-answer-correct 0)
+        (reset! running false)
         (reset! ask-answer true))))
 
 ;; -------------------------
 ;; Event handler
 
+(defn retry-main-loop []
+  (reset! current-answer 0)
+  (reset! last-answer-correct 0)
+  (reset! running true)
+  (reset! ask-answer false)
+  (reset! user-answer "")
+  (main-loop))
+
 (defn start-main-loop []
   (reset! current-answer 0)
   (reset! last-answer-correct 0)
+  (reset! running true)
   (reset! numbers [])
   (reset! ask-answer false)
   (reset! user-answer "")
-  (reset! current-config {:number-of-digits @number-of-digits
-                          :number-of-rows @number-of-rows
-                          :timeout (+ @timeout 100)
-                          :number-shown-ms @number-shown-ms})
   (main-loop))
 
 (defn validate-answer []
@@ -70,6 +84,14 @@
     (do (reset! last-answer-correct 1)
         (swap! history-total-right-answer inc))
     (do (reset! last-answer-correct -1))))
+
+(defn global-handler [e]
+  (when (= "Enter" (.-code e))
+    (when-not (or @running @ask-answer)
+      (js/console.log e)
+      (start-main-loop))))
+
+(js/document.addEventListener "keydown" global-handler)
 
 ;; -------------------------
 ;; Routes
@@ -121,35 +143,42 @@
              :value @number-of-digits
              :min 1
              :max 10
-             :on-change #(reset! number-of-digits (int (-> % .-target .-value)))}]]
+             :on-change #(do (reset! number-of-digits (int (-> % .-target .-value)))
+                             (swap! current-config assoc :number-of-digits @number-of-digits))}]]
    [:p "Number of rows: "
     [:input {:type "number"
              :value @number-of-rows
              :min 2
              :max 100
-             :on-change #(reset! number-of-rows (int (-> % .-target .-value)))}]]
-   [:p "Show number for ms: "
+             :on-change #(do (reset! number-of-rows (int (-> % .-target .-value)))
+                             (swap! current-config assoc :number-of-rows @number-of-rows))}]]
+   [:p "Show number for n ms: "
     [:input {:type "number"
              :value @number-shown-ms
              :min 500
              :max 5000
              :step 500
-             :on-change #(reset! number-shown-ms (int (-> % .-target .-value)))}]]
+             :on-change #(do (reset! number-shown-ms (int (-> % .-target .-value)))
+                             (swap! current-config assoc :number-shown-ms @number-shown-ms))}]]
    [:p "Timeout: "
     [:input {:type "number"
              :value @timeout
              :min 500
              :max 5000
              :step 500
-             :on-change #(reset! timeout (int (-> % .-target .-value)))}]]])
+             :on-change #(do (reset! timeout (int (-> % .-target .-value)))
+                             (swap! current-config assoc :timeout @timeout))}]]])
 
 (defn home-page []
   (fn []
     [:span.main
      [:h1.title "ANZAN"]
      [number-component]
-     [:div
-      [:input {:type "button" :value (if @ask-answer "Skip" "Start") :on-click start-main-loop}]]
+     (when (not @running)
+       [:div
+        [:input {:type "button" :value (if @ask-answer "Skip" "Start") :on-click start-main-loop}]
+        (when (not-empty @numbers)
+          [:input {:type "button" :value "Retry" :on-click retry-main-loop}])])
      [history-component]
      [config-component]]))
 
